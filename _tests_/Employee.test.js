82713ff6cc2758e473f4a553d843911fae19cd1f
Socially Awkward Streams <candice.radam@gmail.com>
2023-01-11 09:37:36 -0600

    Still working on getting tests to pass.

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,30 +1,30 @@
-const Employee = require('../lib/Employee');
+const Employee = require('../lib/employee');
 
 //tests to confirm inputs are completed with expected values. 
 
 describe('employee name entered', () => {
-    const employee = new Employee('Candice', 35, 'candice@gmail.com');
+    const employee = createEmployee('Candice', 35, 'candice@gmail.com');
 
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
 // testing id from getId()
 test('gets employee ID', () => {
-    const employee = new Employee('Candice', 35, 'candice@gmail.com');
+    const employee = createEmployee('Candice', 35, 'candice@gmail.com');
 
     expect(employee.getId()).toEqual(expect.any(Number));
 });
 
 // testing emails from getEmail()
 test('gets employee email', () => {
-    const employee = new Employee('Candice', 35, 'candice@gmail.com');
+    const employee = createEmployee('Candice', 35, 'candice@gmail.com');
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
 });
 
 // testing role from getRole()
 test('gets role of employee', () => {
-    const employee = new Employee('Candice', 35, 'candice@gmail.com');
+    const employee = createEmployee('Candice', 35, 'candice@gmail.com');
 
     expect(employee.getRole()).toEqual("Employee");
 }); 
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,15 +1,15 @@
-const Manager = require('../lib/Manager');
+const Manager = require('../lib/manager');
 
 //tests to confirm objects can be created. 
 
 test('creates a Manager object', () => {
-    const manager = new Manager('Candice', 35, 'candice@gmail.com', 1234);
+    const manager = createManager('Candice', 35, 'candice@gmail.com', 1234);
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // testing role from getRole()
 test('gets role of manager', () => {
-    const manager = new Manager('Candice', 35, 'candice@gmail.com');
+    const manager = createManager('Candice', 35, 'candice@gmail.com');
 
     expect(manager.getRole()).toEqual("Manager");
 }); 
