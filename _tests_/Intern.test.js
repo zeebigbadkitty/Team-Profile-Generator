d9df50532d4912d18e5e15e43244b38e2b47e6b2
Socially Awkward Streams <candice.radam@gmail.com>
2023-01-11 09:14:07 -0600

    Working on getting tests to pass.

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,1 +1,23 @@
 const Intern = require('../lib/Intern');
+
+
+//creating Intern
+
+test('creates an Intern object', () => {
+    const intern = new Intern('Candice', 35, 'candice@gmail.com', 'University of Texas at Austin');
+    expect(intern.school) .toEqual(expect.any(String));
+});
+
+// testing role from getRole()
+test('gets role of intern', () => {
+    const intern = new Intern('Candice', 35, 'candice@gmail.com', 'University of Texas at Austin');
+
+    expect(intern.getRole()).toEqual("Intern");
+}); 
+
+// testing school value from getSchool()
+test('gets school value', () => {
+    const intern = new Intern('Candice', 35, 'candice@gmail.com', 'University of Texas at Austin');
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,16 @@
 const Manager = require('../lib/Manager');
+
+//tests to confirm objects can be created. 
+
+test('creates a Manager object', () => {
+    const manager = new Manager('Candice', 35, 'candice@gmail.com', 1234);
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// testing role from getRole()
+test('gets role of manager', () => {
+    const manager = new Manager('Candice', 35, 'candice@gmail.com');
+
+    expect(manager.getRole()).toEqual("Manager");
+}); 
+
